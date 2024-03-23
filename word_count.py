c682f5b502655293f2014748ad6151f5c31f0d33
Daniel <danielmorernoinf@gmail.com>
2024-03-22 22:36:51 -0300

    se corrige identacion

--- word_count.py
+++ word_count.py
@@ -1,7 +1,7 @@
 from os import system
 
 # Importar el texto
-print("")
+# no supe como dejar que el texto lo tome estando en el mismo directorio
 with open("C:\\Users\\ztyle\\Dropbox\\BootCamp\\Desarrollo\\Modulo_Python\\Desafio4\\lorem_ipsum.txt", "r") as file:
     texto = file.read()
 print(texto)
@@ -9,22 +9,21 @@ print(texto)
 #separando texto en caracteras
 #creo una lista con los caracteres dentro del texto
 caracteres = [caracter for caracter in texto]
+print('caracteres')
 print(caracteres)
 
 #Transformo la lista en set para eliminar elementos duplicados
 set_caracter = set(caracteres)
+('set_caracter')
 print(set_caracter)
 
 #Contando la cantidad de palabras
 cantidad_caracteres = len(set_caracter)
 print(cantidad_caracteres)
 
-
-
 #separando el texto en palabras.
 palabras = texto.split()
 print(palabras) #el resultado se muestra en una lista
-
 
 #Transformo lista a Set para eliminar elementos duplicados
 set_palabras = set(palabras)
@@ -33,7 +32,7 @@ print(set_palabras)
 #Contando la cantidad de palabras
 cantidad_palabras = len(set_palabras)
 
-system('cls')
+
 
 print(f"El número de caracteres distintos es: {cantidad_caracteres}")
 print(f"El número de palabras distintas es: {cantidad_palabras}")
